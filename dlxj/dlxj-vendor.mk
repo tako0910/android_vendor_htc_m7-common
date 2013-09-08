9c4f36124e41da840ae913b441c5a72437f8c3e7
tako0910 <tako.oct0910@gmail.com>
2013-09-09 00:41:23 +0900

    Move dlx to dlxj

--- dlxj/dlxj-vendor.mk
+++ dlxj/dlxj-vendor.mk
@@ -16,4 +16,4 @@
 
 # Pick up overlay for features that depend on non-open-source files
 
-$(call inherit-product, vendor/htc/dlx/dlx-vendor-blobs.mk)
+$(call inherit-product, vendor/htc/dlxj/dlxj-vendor-blobs.mk)
